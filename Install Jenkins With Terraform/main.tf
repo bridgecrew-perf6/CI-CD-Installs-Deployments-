da728459e39eeb4f1ce292bfb7b6b1183d58b156
Cameron Bankwala <98491955+ninjadev25@users.noreply.github.com>
2022-03-29 17:01:32 +0100

    Added files via upload
    1st version of automating Jenkins Installation

---
+++ Install Jenkins With Terraform/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+resource "aws_instance" "example3" {
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  tags = {
+    Name = "terraform-example3"
+  }
+  user_data = file("userdata.sh")
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance3"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
